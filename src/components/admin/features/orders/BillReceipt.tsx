7c68d55940a2afd7d04d49a5315b6ce3ee7a8b19
hoangkhoanguyen <48675300+hoangkhoanguyen@users.noreply.github.com>
2026-09-07 20:29:19 +0700

    fix

--- src/components/admin/features/orders/BillReceipt.tsx
+++ src/components/admin/features/orders/BillReceipt.tsx
@@ -111,7 +111,7 @@ export default function BillReceipt({
     <>
       <style>{printStyles}</style>
       <div className={`bill-root ${fontClassName}`}>
-        <div className="bill hidden">
+        <div className="bill">
           {/* Header */}
           <div className="bill-center">
             <div className="bill-shop-name">{RESTAURANT.name}</div>
